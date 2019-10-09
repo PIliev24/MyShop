32963e450b821b0ec5ecb290cb29f67c582d7dbd
Skino24 <fvagvtv@gmail.com>
2019-10-09 20:46:21 +0300

    Created Basket Model

--- MyShop/MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -9,5 +9,7 @@ namespace MyShop.DataAccess.SQL
         { }
         public DbSet<Product> Product { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
     }
 }
